951a79ae02ee58fcf1feb68a569aaad02074b741
ArthurSlgd <arthurrsalgadoo@gmail.com>
2025-03-11 00:03:47 +0000

    Testes OK

---
+++ pilha/target/debug/deps/lib-6f215081ff675a0d.d
@@ -0,0 +1,5 @@
+/workspaces/Arthur.salgado.3nb/pilha/target/debug/deps/lib-6f215081ff675a0d: tests/lib.rs
+
+/workspaces/Arthur.salgado.3nb/pilha/target/debug/deps/lib-6f215081ff675a0d.d: tests/lib.rs
+
+tests/lib.rs:
---
+++ pilha/target/debug/deps/pilha-0ad760cb20aa20f2.d
@@ -0,0 +1,5 @@
+/workspaces/Arthur.salgado.3nb/pilha/target/debug/deps/pilha-0ad760cb20aa20f2: src/main.rs
+
+/workspaces/Arthur.salgado.3nb/pilha/target/debug/deps/pilha-0ad760cb20aa20f2.d: src/main.rs
+
+src/main.rs:
